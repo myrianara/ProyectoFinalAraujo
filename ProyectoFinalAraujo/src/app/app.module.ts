08b56dbff12f5a5c7c2bc58c6bc87c566a928d97
myrianara <myrianaraujo10@gmail.com>
2022-10-21 00:48:46 -0300

    Creacion del header

--- ProyectoFinalAraujo/src/app/app.module.ts
+++ ProyectoFinalAraujo/src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ComponentsComponent } from './components/components.component';
+import { HeaderComponent } from './components/header/header.component';
+
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentsComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    LogoAPComponent,
+    SocialComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule
