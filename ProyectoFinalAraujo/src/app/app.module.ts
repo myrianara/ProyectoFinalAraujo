9a14d6e91e77b1d7ca9163f23a3e8acb70821ee6
myrianara <myrianaraujo10@gmail.com>
2022-10-25 15:17:57 -0300

    Soft y Hard Skills

--- ProyectoFinalAraujo/src/app/app.module.ts
+++ ProyectoFinalAraujo/src/app/app.module.ts
@@ -11,6 +11,9 @@ import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
+import { HysComponent } from './components/hys/hys.component';
+
+
 
 @NgModule({
   declarations: [
@@ -23,12 +26,16 @@ import { EducacionComponent } from './components/educacion/educacion.component';
     BannerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
-    EducacionComponent
+    EducacionComponent,
+    HysComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
